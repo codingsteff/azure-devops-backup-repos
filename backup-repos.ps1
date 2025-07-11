fea2bc31f95de14d636649a6a84c4d27dac8b248
Stefan B. <codingsteff@users.noreply.github.com>
2025-07-11 15:33:00 +0200

    refactor: divide into smaller functions

--- backup-repos.ps1
+++ backup-repos.ps1
@@ -15,6 +15,18 @@ function Get-Repos($project) {
     return (az repos list --project $project.id -o json | ConvertFrom-Json) | Sort-Object name
 }
 
+function Get-Branches($repo) {
+    # List branch refs
+    $url = $repo.remoteUrl
+    $cmd = "git ls-remote --heads $url"
+    $refs = Invoke-AuthenticatedGitCommand $cmd $personalAccessToken
+
+    # Extract branch names and filter based on the provided regex pattern
+    return $refs |
+    ForEach-Object { ($_ -split "\s+")[1] -replace "^refs/heads/", "" } |
+    Where-Object { $_ -match $branchNameRegex }
+}
+
 function Add-Token($url, $token) {
     if ($token) {
         $delimiter = $url.indexOf("@")
@@ -39,65 +51,67 @@ function Invoke-AuthenticatedGitCommand($command, $token) {
 
 function Backup-Repo($project, $repo) {
     $repoName = $repo.name
-    $projectName = $project.name
-    $url = $repo.remoteUrl
     Write-Host "   " $repoName
-    $existRepo = Test-Path $repoName
 
-    if ($backupAllBranches) {
-        # Back up all branches
-        if (!($existRepo)) {
-            Add-Directory $repoName
-        }
-        # Save current directory to return to it later
-        $currentDir = Get-Location
-        try {
-            Set-Location -Path $repoName
-
-            # List branch refs
-            $cmd = "git ls-remote --heads $url"
-            $refs = Invoke-AuthenticatedGitCommand $cmd $personalAccessToken
-
-            # Extract branch names and filter based on the provided regex pattern
-            $branches = $refs |
-                ForEach-Object { ($_ -split "\s+")[1] -replace "^refs/heads/", "" } |
-                Where-Object { $_ -match $branchNameRegex }
-
-            if (-not $branches) {
-                Write-Warning "      No branches matched the filter '$branchNameRegex'."
-                return
-            }
-            
-            # Clone each branch into its own folder
-            foreach ($branch in $branches) {
-                # Replace all punctuation (except letters, numbers, and underscore) with "_"
-                $branchDir = "$branch" -replace '[^\w]', '_'
-
-                $existBranch = Test-Path $branchDir
-                if ($existBranch) {
-                    # Fetch changes and update the local branch
-                    $cmd = "git -C '$branchDir' pull $url $branch"
-                }
-                else {   
-                    # Clone the remote branch into a new directory       
-                    $cmd = "git clone --single-branch --branch $branch $url '$branchDir'"
-                }
-                Invoke-AuthenticatedGitCommand $cmd $personalAccessToken
-            }
-        }
-        finally {
-            # Always return to previous directory
-            Set-Location $currentDir
-        }
-    }
-    else {
+    if (!$backupAllBranches) {
         # Back up main branch only
-        if ($existRepo) {
-            # Invoke-Expression "git remote prune origin"
-            $cmd = "git -C $repoName pull $url"
+        Backup-MainBranch $repo
+        return
+    }
+
+    # Back up all branches
+    $existRepo = Test-Path $repoName
+    if (!($existRepo)) {
+        Add-Directory $repoName
+    }
+    # Save current directory to return to it later
+    $currentDir = Get-Location
+    try {
+        Set-Location -Path $repoName
+
+        # Get all branches from repository and matching the regex pattern
+        $branches = Get-Branches $repo            
+        if (-not $branches) {
+            Write-Warning "      No branches matched the filter '$branchNameRegex'."
+            return
         }
-        else {          
-            $cmd = "git clone $url $repoName"
+            
+        # Back up each branch into its own folder
+        Backup-Branches $repo $branches            
+    }
+    finally {
+        # Always return to previous directory
+        Set-Location $currentDir
+    } 
+}
+
+function Backup-MainBranch($repo) {
+    $repoName = $repo.name
+    $url = $repo.remoteUrl    
+    if ($existRepo) {
+        # Invoke-Expression "git remote prune origin"
+        $cmd = "git -C $repoName pull $url"
+    }
+    else {          
+        $cmd = "git clone $url $repoName"
+    }
+    Invoke-AuthenticatedGitCommand $cmd $personalAccessToken
+}
+
+function Backup-Branches($repo, $branches) {
+    $url = $repo.remoteUrl
+    foreach ($branch in $branches) {
+        # Replace all punctuation (except letters, numbers, and underscore) with "_"
+        $branchDir = "$branch" -replace '[^\w]', '_'
+
+        $existBranch = Test-Path $branchDir
+        if ($existBranch) {
+            # Fetch changes and update the local branch
+            $cmd = "git -C '$branchDir' pull $url $branch"
+        }
+        else {   
+            # Clone the remote branch into a new directory       
+            $cmd = "git clone --single-branch --branch $branch $url '$branchDir'"
         }
         Invoke-AuthenticatedGitCommand $cmd $personalAccessToken
     }
